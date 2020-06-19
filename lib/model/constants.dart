f411ecc169004607bcb85fbcf05d7958ead85178
FozanKh <Fozan.kh@icloud.com>
2020-06-19 20:48:43 +0300

    Fix App Icon

--- lib/model/constants.dart
+++ lib/model/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:qibla_plus/model/components/tips.dart' as Tips;
 
 const kKabbahLat = 0.3738927226761722; //21.4224750 deg
 const kKabbahLon = 0.6950985611585316; //39.8262139 deg
@@ -41,7 +40,7 @@ const kEnLangString = 'English';
 const kEnTitle = 'Qibla +';
 const kArTitle = '+ قبلة';
 
-const kArTips = ": نصائح لقبلة أدق\n  عاير البوصلة بتدوير الجهاز على شكل 8 ♾️\n  ابتعد عن "
+const kArTips = ": نصائح لقبلة أدق\n  عاير البوصلة بتدوير الجهاز على شكل الرقم 8 ♾️\n  ابتعد عن "
     "الأجهزة الإلكترونية 🧲\n ضع هاتفك بشكل مسطح📱";
 
 const kEnTips = 'Tips for better qibla accuracy:\n'
@@ -49,14 +48,14 @@ const kEnTips = 'Tips for better qibla accuracy:\n'
     '🧲\tMove away from electronic devices\n'
     '📱\tLay your device flat';
 
-const kArPermissionErr = '⚠\nالرجاء إعطاء هذا التطبيق صلاحيات الموقع "أثناء الأستخدام" لمعرفة القبلة';
-const kEnPermissionErr = "⚠\nPlease allow this app 'While using the app' location privileges to "
+const kArPermissionErr = '⚠️\nالرجاء إعطاء هذا التطبيق صلاحيات الموقع "أثناء الأستخدام" لمعرفة القبلة';
+const kEnPermissionErr = "⚠️\nPlease allow this app 'While using the app' location privileges to "
     "determine qibla";
 
-const kArLocationServicesErr = "⚠\nالرجاء تفعيل خدمات الموقع من الإعدادات لمعرفة القبلة";
-const kEnLocationServicesErr = "⚠\nPlease enable location services from your device's setting to determine qibla";
+const kArLocationServicesErr = "⚠️\nالرجاء تفعيل خدمات الموقع من الإعدادات لمعرفة القبلة";
+const kEnLocationServicesErr = "⚠️\nPlease enable location services from your device's setting to determine qibla";
 
-const kArAmbiguousErr = "⚠\nعذراً، حدث خطأ في تحديد القبلة، الرجاء إعادة تشغيل التطيبق";
-const kEnAmbiguousErr = "⚠\nSorry, an error has occur please restart the app";
+const kArAmbiguousErr = "⚠️\nعذراً، حدث خطأ في تحديد القبلة، الرجاء إعادة تشغيل التطيبق";
+const kEnAmbiguousErr = "⚠️\nSorry, an error has occur please restart the app";
 
 const kErrText = Text(kEnAmbiguousErr, style: kErrTextStyle, textAlign: TextAlign.center);
